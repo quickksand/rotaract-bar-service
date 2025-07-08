f5c8220c0c536b1bc6612e58c5ec470dc4011f88
Leon Lichius <leon.lichius@mailbox.org>
2025-07-08 10:33:56 +0200

    refactorings + add openapi

--- src/main/java/com/lichius/rac/ansbach/altstadtfest/Application.java
+++ src/main/java/com/lichius/rac/ansbach/altstadtfest/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AltstadtfestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AltstadtfestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
